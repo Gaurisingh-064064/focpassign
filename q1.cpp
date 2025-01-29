0a6c078ad8598674d6b8490fdda068c453d6bd9e
Gaurisingh-064064 <24csu064@ncuindia.edu>
2025-01-29 09:37:07 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    string name;
+    cout<<"\n enter name :";
+    cin>>name;
+    cout<<"hello "<<name <<"you are" <<age;
+    return 0;
+}
